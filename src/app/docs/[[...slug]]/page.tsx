78f038ce7c46aa84d694ecb2559f065f5f84342f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-24 22:10:09 +0530

    :zap: :art: catch all segment with default docs page using two square brackets

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,7 +1,7 @@
 export default async function Docs({
   params,
 }: {
-  params: Promise<{ slug: string[] }>;
+  params: Promise<{ slug?: string[] }>;
 }) {
   const { slug } = await params;
   console.log(slug);
