6a463b9c03cc88507f62df5ba43783caa4ae25e0
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-24 23:51:08 +0530

    :zap: :art: Not found message specific to product id and review id

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function NotFound() {
+  const path = usePathname();
+
+  const productId = path.split("/")[2];
+  const reviewId = path.split("/")[4];
+
+  console.log(path);
   return (
     <div>
       <h2>Review Not Found</h2>
-      <p>Could not found specified review</p>
+      <p>
+        Review {reviewId} not found for product {productId}
+      </p>
     </div>
   );
 }
