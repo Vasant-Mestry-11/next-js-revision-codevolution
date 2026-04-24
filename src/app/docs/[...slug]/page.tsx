1667caaed28c847eaa509b1cd0925a896302faf5
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-24 22:00:34 +0530

    :zap: :art: catch all segments

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,29 @@
+export default async function Docs({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>;
+}) {
+  const { slug } = await params;
+  console.log(slug);
+
+  if (slug?.length === 2) {
+    return (
+      <div>
+        <h2>
+          Viewing feature {slug[0]} concept {slug[1]}
+        </h2>
+      </div>
+    );
+  } else if (slug?.length === 1) {
+    return (
+      <div>
+        <h2>Viewing feature {slug[0]}</h2>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>Docs page</h2>
+    </div>
+  );
+}
