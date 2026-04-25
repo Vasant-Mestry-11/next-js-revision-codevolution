87fcff8f25153f5c4478ff47cc4d1179d1235fcf
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-25 20:04:03 +0530

    :zap: :art: Products Item link added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,9 +7,15 @@ export default function ProductsList() {
         <Link href="/">Home</Link>
       </div>
       <h1>Products List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 3</Link>
+      </h2>
     </div>
   );
 }
