798cece95ddad00b25b846d7b59343f1cd6baca5
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-04-25 17:36:57 +0530

    :zap: :art: dynamic metadata using generateMetadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,20 @@
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productId } = await params;
+
+  return {
+    title: `Product ${productId}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const { productId } = await params;
   return (
     <div>
