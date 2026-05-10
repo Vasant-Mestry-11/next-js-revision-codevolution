dde3ca379e3df8fcdc929a0e632aabe43e626844
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-05-10 20:17:10 +0530

    :zap: :art: params and searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,11 @@ export default function Home() {
       <div>
         <Link href="/blog">Blog</Link>
         <Link href="/products">Products</Link>
+
+        <br />
+
+        <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+        <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
       </div>
     </div>
   );
