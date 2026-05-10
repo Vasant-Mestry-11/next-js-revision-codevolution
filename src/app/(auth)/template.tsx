3cc11ddb537994863d2f9fe8fafd5bcc5491aea0
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-05-10 21:21:53 +0530

    :zap: :art: replace template with layout, so state wont be persistent

---
+++ src/app/(auth)/template.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
+import { useState } from "react";
+
+const navlinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [input, setInput] = useState("");
+  const pathname = usePathname();
+
+  return (
+    <div>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
+      {navlinks.map((link) => {
+        const isActive =
+          pathname === link.href ||
+          (pathname.startsWith(link.href) && link.href !== "/");
+        return (
+          <Link
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            key={link.name}
+            href={link.href}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+}
